b0a50f2d6b7c9451dfc21880f76c19e9c93e0b58
Tomohiro Suwa <neoen.gsn@gmail.com>
2013-08-25 04:28:40 +0900

    bug fix

--- setup.sh
+++ setup.sh
@@ -7,5 +7,5 @@ do
 	ln -s $HOME/github/dotfiles/$file $HOME/$file
 done
 
-[! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-[! -d ~/.vim/neobundle.vim ] && git clone git://github.com/Shougo/neobundle.vim ~/.vim/neobundle.vim
+[ ! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+[ ! -d ~/.vim/neobundle.vim ] && git clone git://github.com/Shougo/neobundle.vim ~/.vim/neobundle.vim
