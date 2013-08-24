6d75bbf982e520a7667941ce4a2f3a3cf484a9fb
Tomohiro Suwa <neoen.gsn@gmail.com>
2013-08-25 03:18:54 +0900

    introduce setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DOT_FILES=( .zshrc .vimrc .gitconfig .gitignore )
+
+for file in ${DOT_FILES[@]}
+do
+	ln -s $HOME/github/dotfiles/$file $HOME/$file
+done
+
+[! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+[! -d ~/.vim/neobundle.vim ] && git clone git://github.com/Shougo/neobundle.vim ~/.vim/neobundle.vim
