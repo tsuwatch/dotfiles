80429597cf68355580c22c54127679afe7d94f8a
Tomohiro Suwa <neoen.gsn@gmail.com>
2014-04-07 16:14:22 +0900

    Modify ln -s .vim

--- setup.sh
+++ setup.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-DOT_FILES=( .gemrc .zshrc .zshrc.custom .vim .vimrc .gitconfig .gitignore )
+DOT_FILES=( .gemrc .zshrc .zshrc.custom .vimrc .gitconfig .gitignore )
+DIR=$(pwd)
+
+if [ -e $HOME/.vim ]
+then
+  for filepath in $DIR/.vim/*
+  do
+    filename=`basename $filepath`
+    rm -r $HOME/.vim/$filename
+    ln -s $filepath $HOME/.vim/$filename
+  done
+else
+  ln -s $DIR/.vim $HOME/.vim
+fi
 
 for file in ${DOT_FILES[@]}
 do
-	ln -s $HOME/github/dotfiles/$file $HOME/$file
+	ln -s $DIR/$file $HOME/$file
 done
+
 
 [ ! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 [ ! -d ~/.vim/neobundle.vim ] && git clone git://github.com/Shougo/neobundle.vim ~/.vim/neobundle.vim
