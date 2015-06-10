2fd7477d97836f7ed5bf686d4fe2bea77c270158
Tomohiro Suwa <neoen.gsn@gmail.com>
2015-06-10 13:19:45 +0900

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -3,23 +3,10 @@
 DOT_FILES=( .gemrc .zshrc .zshrc.custom .vimrc .vim .gitconfig .gitignore .tmux.conf )
 DIR=$(pwd)
 
-#if [ -d $HOME/.vim ]
-#then
-#  for filepath in $DIR/.vim/*
-#  do
-#    filename=`basename $filepath`
-#    rm -rf $HOME/.vim/$filename
-#    ln -s $filepath $HOME/.vim/$filename/
-#  done
-#else
-#  ln -s $DIR/.vim $HOME/.vim
-#fi
-
 for file in ${DOT_FILES[@]}
 do
 	ln -s $DIR/$file $HOME/$file
 done
-
 
 [ ! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 [ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
